4665d1416b1106b79f5f04829718796b27fc221e
Rohan-cp <rohancp9@icloud.com>
2021-05-12 10:23:20 +0530

    added location API to get user location in NewPlaceScreen

--- components/LocationSelector.js
+++ components/LocationSelector.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Button, Text, StyleSheet, ActivityIndicator, Alert } from 'react-native';
 import Colors from '../constants/Colors';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 const LocationSelector = props => {
-  const getLocationHandler = () => {
+  const [ isFetching, setIsFetching ] = useState(false);
+  const [ locationPicked, setLocationPicked ] = useState();
+  const verifyPermissions = async () => {
+    const result = await Permissions.askAsync(Permissions.LOCATION_FOREGROUND);
+    if (result.status != 'granted') {
+      Alert.alert('Insufficient permissions!',
+      'You need to grant location permissions to use this app.',
+      [{text: 'Okay'}]);
+      return false;
+    }
+    return true;
+  };
 
+  const getLocationHandler = async () => {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) {
+      return;
+    }
+    try {
+      setIsFetching(true);
+      const location = await Location.getCurrentPositionAsync({ timeout: 5000 });
+      console.log(location);
+      setLocationPicked({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude
+      });
+    } catch (error) {
+      Alert.alert(
+        'Could not fetch location', 
+        'Please try again later or pick a location on the map', 
+        [{ text: okay }])
+    }
+    setIsFetching(false);
   };
 
   return (
     <View style={styles.locationSelector} >
       <View style={styles.mapPreview}>
-        <Text>No location chosen text!</Text>
+        {isFetching ? <ActivityIndicator size='large' color={Colors.primary} /> : <Text>No location chosen text!</Text>}
       </View>
       <Button title="Get User Location" color={Colors.primary} onPress={getLocationHandler} />
     </View>
@@ -27,6 +60,8 @@ const styles = StyleSheet.create({
     height: 150,
     borderColor: '#ccc',
     borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 });
 
